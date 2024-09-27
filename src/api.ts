ee725ef756282d4125a2324b66fd4acd8aa5cef2
JMC816 <jangmincheol816@gmail.com>
2024-09-27 15:56:59 +0900

    fix: CORS 문제 해결을 위한 경로 수정

--- src/api.ts
+++ src/api.ts
@@ -1,14 +1,12 @@
 export function GetCook() {
   return fetch(
-    `http://openapi.foodsafetykorea.go.kr/api/${
-      import.meta.env.VITE_API_KEY
-    }/COOKRCP01/json/1/1000`
+    `/api/api/${import.meta.env.VITE_API_KEY}/COOKRCP01/json/1/1000`
   ).then((response) => response.json());
 }
 
 export function GetIngredient(ingredient: string) {
   return fetch(
-    `http://openapi.foodsafetykorea.go.kr/api/${
+    `/api/api/${
       import.meta.env.VITE_API_KEY
     }/COOKRCP01/json/1/1000/RCP_PARTS_DTLS="${ingredient}"`,
     {
